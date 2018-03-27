e7def5a9a4441b1b234c75ae4f04ccbd992ef570
Bertocq <bertocq@gmail.com>
2018-03-28 00:17:40 +0200

    Replace Date.new(X) for Time.zone.local(X).to_date
    Why:
    
    Date.new does not take into account the current timezone, while other
    parts of the application do. By default always parsing any date with the
    default timezone and converting the resulting Time to Date would prevent
    this kind of issues
    
    How:
    
    Replacing any `Date.new(...)` with `Time.zone.local(...).to_date`

--- lib/census_api.rb
+++ lib/census_api.rb
@@ -23,7 +23,7 @@ class CensusApi
       str = data[:datos_habitante][:item][:fecha_nacimiento_string]
       day, month, year = str.match(/(\d\d?)\D(\d\d?)\D(\d\d\d?\d?)/)[1..3]
       return nil unless day.present? && month.present? && year.present?
-      Date.new(year.to_i, month.to_i, day.to_i)
+      Time.zone.parse(year.to_i, month.to_i, day.to_i)
     end
 
     def postal_code
